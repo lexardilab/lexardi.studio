60495ca9257c6be251aa679ea0efb7f774a73421
lexardi <lexardi.lab@gmail.com>
2025-01-09 20:26:36 +0100

    header

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <>
             <div className='flex justify-center items-center h-screen bg-yellow-200'>
-                <Image src='/Logo_Harri.svg' width='10000' height='200' alt='Harri Brand'></Image>
+                <Image src='/harri+lexardi.svg' width='10000' height='200' alt='Harri Brand'></Image>
             </div>
         </>
     );
